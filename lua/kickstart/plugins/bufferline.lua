7bbfa78ade050a8ab99d964f90509de66777a8c9
fmparma <zhengliming2004@sina.com>
2024-12-09 22:23:57 +0800

    feat():[#]Split plugins into independent files

---
+++ lua/kickstart/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+--  bufferline setting
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require('bufferline').setup {
+      options = {
+        offsets = {
+          {
+            filetype = 'NvimTree',
+            text = 'File Explorer',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
+      },
+    }
+  end,
+}
